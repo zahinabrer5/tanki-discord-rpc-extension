ab4868b5c6bd0a06f8fe9d57acc3677dbb51e289
Zahin <zahinabrer5@gmail.com>
2025-01-01 21:29:38 -0500

    improve security by sending an API key in the request headers

--- background.js
+++ background.js
@@ -1,43 +1,82 @@
+// Function to get API key and make the request
+function startRPC() {
+    // Retrieve the API key from storage
+    chrome.storage.local.get('apiKey', function (data) {
+        const apiKey = data.apiKey;
+
+        if (!apiKey) {
+            console.error('API Key not found');
+            return;
+        }
+
+        fetch('http://localhost:3000/startRPC', {
+            method: 'GET',
+            headers: {
+                'x-api-key': apiKey,
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.text())
+            .then(data => {
+                console.log('RPC started:', data);
+            })
+            .catch(error => {
+                console.error('Error starting RPC:', error);
+            });
+    });
+}
+
+// Function to get API key and make the request
+function stopRPC() {
+    // Retrieve the API key from storage
+    chrome.storage.local.get('apiKey', function (data) {
+        const apiKey = data.apiKey;
+
+        if (!apiKey) {
+            console.error('API Key not found');
+            return;
+        }
+
+        fetch('http://localhost:3000/stopRPC', {
+            method: 'GET',
+            headers: {
+                'x-api-key': apiKey,
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.text())
+            .then(data => {
+                console.log('RPC stopped:', data);
+            })
+            .catch(error => {
+                console.error('Error stopping RPC:', error);
+            });
+    });
+}
+
 let openTabs = {};
 
 // Listen for when a tab is created
-chrome.tabs.onCreated.addListener(function(tab) {
-    console.log("Tab created: ", tab);
+chrome.tabs.onCreated.addListener(function (tab) {
+    // console.log("Tab created: ", tab);
     // Now, let's listen for the tab to finish loading
-    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         // We're checking if the tab has finished loading and has the correct URL
         if (tabId === tab.id && changeInfo.status === "complete" && tab.url && tab.url.includes("tankionline.com/play")) {
             // console.log("Tanki Online page opened and fully loaded!");
             openTabs[tab.id] = tab.url; // add to our list of open tabs
-            fetch('http://localhost:3000/startRPC')
-                .then(response => response.json())
-                .then(data => {
-                    console.log("Started RPC", data);
-                })
-                .catch(err => {
-                    console.error("Error starting RPC", err);
-                });
+            startRPC();
         }
     });
 });
 
 // Listen for when a tab is removed (closed)
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-    console.log("Tab removed: ", tabId);
-    
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+    // console.log("Tab removed: ", tabId);
     // Check if the removed tab was a Tanki Online tab using the openTabs map
     if (openTabs[tabId] && openTabs[tabId].includes("tankionline.com/play")) {
         // console.log("Tanki Online tab closed, stopping RPC.");
-        
-        fetch('http://localhost:3000/stopRPC')
-            .then(response => response.json())
-            .then(data => {
-                console.log("Stopped RPC", data);
-            })
-            .catch(err => {
-                console.error("Error stopping RPC", err);
-            });
-        
+        stopRPC();
         // Remove the tab from the openTabs tracker after it's closed
         delete openTabs[tabId];
     } else {
